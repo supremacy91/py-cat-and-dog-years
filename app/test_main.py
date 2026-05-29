5fd10694b73947ef66600faec1eda9da966bb53c
Maks Kaplunovskiy <kaplunovskymv@gmail.com>
2026-05-29 22:34:22 +0300

    implement py-cat-and-dog-years

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,26 @@
-from app.main import get_human_age
+from app import main
 
-# write your code here
+
+def test_should_return_zero_when_age_less_than_fifteen() -> None:
+    assert main.get_human_age(14, 14) == [0, 0]
+
+
+def test_should_return_one_human_year_after_fifteen_pet_years() -> None:
+    assert main.get_human_age(15, 15) == [1, 1]
+
+
+def test_should_return_one_human_year_until_twenty_three_pet_years() -> None:
+    assert main.get_human_age(23, 23) == [1, 1]
+
+
+def test_should_return_two_human_years_after_twenty_four_pet_years() -> None:
+    assert main.get_human_age(24, 24) == [2, 2]
+
+
+def test_should_convert_cat_and_dog_years_correctly_after_twenty_four() \
+        -> None:
+    assert main.get_human_age(28, 28) == [3, 2]
+
+
+def test_should_convert_large_ages_correctly() -> None:
+    assert main.get_human_age(100, 100) == [21, 17]
